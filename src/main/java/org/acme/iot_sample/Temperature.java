6b93f26ff661c8484561b0bc9ee8d221a9f0c20d
Maciej Swiderski <swiderski.maciej@gmail.com>
2021-02-25 10:57:10 +0100

    IoT sample - part 1

---
+++ src/main/java/org/acme/iot_sample/Temperature.java
@@ -0,0 +1,26 @@
+package org.acme.iot_sample;
+
+public class Temperature {
+
+	private double value;
+	
+	private String room;
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public String getRoom() {
+		return room;
+	}
+
+	public void setRoom(String room) {
+		this.room = room;
+	}
+	
+	
+}
